93cbf58ba65978ccb947f149eda6e71c34eabc30
keahorvath <keahorvath@outlook.com>
2023-03-10 15:59:08 +0100

    debut

--- backend/src/test/java/pdl/backend/BackendApplicationTests.java
+++ backend/src/test/java/pdl/backend/BackendApplicationTests.java
@@ -1,13 +1,19 @@
 package pdl.backend;
 
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class BackendApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
+    @Test
+    @Order(1)
+    public void appLaunchShouldReturnError() throws Exception {
+
+    }
 }
