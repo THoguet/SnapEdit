df871d42bb04184de00be5e6901eb01a1c9c34f4
keahorvath <keahorvath@outlook.com>
2023-03-13 19:55:03 +0100

    removed unnecessary lines

--- backend/src/main/java/pdl/backend/ImageDao.java
+++ backend/src/main/java/pdl/backend/ImageDao.java
@@ -10,7 +10,6 @@ import java.util.Map;
 import java.util.Objects;
 import java.util.Optional;
 
-import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Repository;
 
 @Repository
