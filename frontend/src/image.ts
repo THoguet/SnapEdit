31e570f57350613817bab8d6df8de07e515b2de6
Theophyl Hoguet <theophyl.hoguet@etu.u-bordeaux.fr>
2023-03-16 01:33:14 +0100

    add data for caching and modifed to know if it's a modifed image or not (to apply algorithm)

--- frontend/src/image.ts
+++ frontend/src/image.ts
@@ -1,13 +1,19 @@
 export interface ImageType {
 	id: number;
 	name: string;
+	data: string;
+	modifed: Map<string, string>;
 }
 
 export class ImageClass implements ImageType {
-	id: number
-	name: string
-	constructor(id: number, title: string) {
+	id: number;
+	name: string;
+	data: string;
+	modifed: Map<string, string>;
+	constructor(id: number, title: string, data: string = "", modifed: Map<string, string> = new Map<string, string>()) {
 		this.id = id
 		this.name = title
+		this.data = data
+		this.modifed = modifed
 	}
 }
