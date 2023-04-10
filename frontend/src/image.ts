8ba59475f5574e09f9b9243d62bd0590c6000af8
Théophyl Hoguet <theophyl.hoguet@etu.u-bordeaux.fr>
2023-04-10 23:38:11 +0200

    add new api field

--- frontend/src/image.ts
+++ frontend/src/image.ts
@@ -2,15 +2,22 @@ export interface ImageType {
 	id: number;
 	name: string;
 	data: string;
+	filtered: number;
 }
 
 export class ImageClass implements ImageType {
 	id: number;
 	name: string;
 	data: string;
-	constructor(id: number, title: string, data: string = "") {
+	filtered: number;
+	constructor(id: number, title: string, data: string = "", filtered: number = -1) {
 		this.id = id
 		this.name = title
 		this.data = data
+		this.filtered = filtered
 	}
+}
+// return true if a and b are equal (ignoring data)
+export function imageEquals(a: ImageType, b: ImageType): boolean {
+	return a.id === b.id && a.name === b.name && a.filtered === b.filtered
 }
